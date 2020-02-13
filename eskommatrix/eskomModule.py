ea0211f491b3d493cfa372107bcf4b97012012a8
Pilasande <Philasandepakkies@gmail.com>
2020-02-13 14:23:24 +0200

    Update eskomModule.py
    function 6 added with doc string

--- eskommatrix/eskomModule.py
+++ eskommatrix/eskomModule.py
@@ -128,7 +128,7 @@ It takes a pandas dataframe as an input.
 The dataframe contains a column, named 'Tweets'.
 The function splits the sentences in the 'Tweets' into a list of seperate words, and place the result into a new column named 'Split Tweets'. The resulting words must all be lowercase!
 The function modifies the input dataframe directly.
-The function then returns the modified dataframe.
+The function then returns the modified dataframe as an output.
     """
     df['Split Tweets'] = df.Tweets.apply(lambda x: x.lower().split())
     return df
