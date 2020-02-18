60394162976e885b304adc11de6348bbd6aa5e43
Pilasande <60289751+Pilasande@users.noreply.github.com>
2020-02-18 13:48:18 +0200

    updated the doctring on the five num summary

--- eskommatrix/eskomModule.py
+++ eskommatrix/eskomModule.py
@@ -29,15 +29,14 @@ def dictionary_of_metrics(items):
          'std': 10400.01,
          'min': 8842.0,
          'max': 39660.0}
-
     """
 
     metric_dict =  {'mean':round(np.mean(items),2),
-                 'median':round(np.median(items),2),
-                 'var':round(np.var(items, ddof=1),2),
-                 'std':round(np.std(items, ddof=1),2),
-                 'min':round(np.min(items),2),
-                 'max':round(np.max(items),2)}
+                    'median':round(np.median(items),2),
+                    'var':round(np.var(items, ddof=1),2),
+                    'std':round(np.std(items, ddof=1),2),
+                    'min':round(np.min(items),2),
+                    'max':round(np.max(items),2)}
     return metric_dict
 ### END FUNCTION
 #----------------------------------------------------------------------------------#
