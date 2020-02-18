3ee004634bb63ebe0e8928495ea6468a35d456ae
sjncube <micanjohnscoderx@gmail.com>
2020-02-18 16:49:43 +0200

    dictionary_of_metrics function is PEP8 complient

--- eskommatrix/eskomModule.py
+++ eskommatrix/eskomModule.py
@@ -31,13 +31,14 @@ def dictionary_of_metrics(items):
          'max': 39660.0}
     """
 
-    metric_dict =  {'mean':round(np.mean(items),2),
-                    'median':round(np.median(items),2),
-                    'var':round(np.var(items, ddof=1),2),
-                    'std':round(np.std(items, ddof=1),2),
-                    'min':round(np.min(items),2),
-                    'max':round(np.max(items),2)}
+    metric_dict = {'mean': round(np.mean(items), 2),
+                   'median': round(np.median(items), 2),
+                   'var': round(np.var(items, ddof=1), 2),
+                   'std': round(np.std(items, ddof=1), 2),
+                   'min': round(np.min(items), 2),
+                   'max': round(np.max(items), 2)}
     return metric_dict
+
 ### END FUNCTION
 #------------------------------------------------------------------------------------#
 ### START FUNCTION
